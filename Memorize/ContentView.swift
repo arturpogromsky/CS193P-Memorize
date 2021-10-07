c6d6f2eec76299b2b6f39e3bf548caf7a559d68f
Artur Pogromsky <ar.pogromsky@gmail.com>
2021-10-07 10:55:21 +0300

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,133 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Артур Погромский on 04.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  @State var emojis = EmojiStore.vehicles
+  @State var emojiCount = 16
+  var body: some View {
+    VStack(alignment: .center) {
+      Text("Memorize!")
+      ScrollView {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+          ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+            CardView(content: emoji)
+          }
+        }
+      }
+      .foregroundColor(.orange)
+      Spacer()
+      ThemeChoser(emojis: $emojis)
+    }
+    .font(.largeTitle)
+    .padding(.horizontal)
+  }
+  
+  var plusButton: some View {
+    Button {
+      if emojiCount < emojis.count {
+        emojiCount += 1
+      }
+    } label: {
+      Image(systemName: "plus.circle")
+        .font(.largeTitle)
+    }
+  }
+  
+  var minusButton: some View {
+    Button {
+      if emojiCount > 1 {
+        emojiCount -= 1
+      }
+    } label: {
+      Image(systemName: "minus.circle")
+        .font(.largeTitle)
+    }
+  }
+}
+
+struct CardView: View {
+  let content: String
+  @State private var isFaceUp = false
+  var body: some View {
+    ZStack {
+      let cornerRadius: CGFloat = 15
+      let roundedRectangle = RoundedRectangle(cornerRadius: cornerRadius)
+      roundedRectangle
+        .strokeBorder(lineWidth: 3)
+        .background(Color.white)
+        .cornerRadius(15)
+      Text(content)
+      if isFaceUp {
+        roundedRectangle
+      }
+    }
+    .onTapGesture { isFaceUp.toggle() }
+    .aspectRatio(2/3, contentMode: .fill)
+  }
+}
+
+struct ThemeChoser: View {
+  @Binding var emojis: [String]
+  var body: some View {
+    HStack(alignment: .firstTextBaseline) {
+      ThemeChoserButton(emojis: $emojis, selectedEmojis: EmojiStore.vehicles, imageName: "car", description: "Vehicles")
+      Spacer()
+      ThemeChoserButton(emojis: $emojis, selectedEmojis: EmojiStore.flags, imageName: "flag", description: "Flags")
+        .padding(.trailing)
+      Spacer()
+      ThemeChoserButton(emojis: $emojis, selectedEmojis: EmojiStore.fruits, imageName: "circle.fill", description: "Fruits")
+    }
+  }
+}
+
+struct ThemeChoserButton: View {
+  @Binding var emojis: [String]
+  var selectedEmojis: [String]
+  var imageName, description: String
+  var body: some View {
+    Button {
+      emojis = selectedEmojis.shuffled()
+    } label: {
+      VStack {
+        Image(systemName: imageName)
+        Text(description)
+          .font(.subheadline)
+          .foregroundColor(.accentColor)
+      }
+    }
+  }
+}
+
+
+struct EmojiStore {
+  static let vehicles = ["🚲", "🚂", "🚁", "🚜", "🚕", "🏎", "🚑", "🚓", "🚒", "✈️", "🚀", "⛵️", "🛸", "🛶", "🚌", "🏍", "🛺", "🚠", "🛵", "🚗", "🚚", "🚇", "🚙", "🚝"]
+  static let fruits = ["🍏", "🍎", "🍐", "🍊", "🍋", "🍌", "🍈", "🍉", "🍇", "🍓", "🫐", "🍈", "🍒", "🍑", "🥭", "🍍", "🥝"]
+  static let flags = ["🏳️‍⚧️", "🇺🇳", "🇦🇿", "🇧🇾", "🇧🇪", "🇧🇴", "🇧🇷", "🇬🇧", "🇨🇦", "🇺🇸", "🇺🇦", "🇨🇭", "🇰🇷", "🇷🇺", "🇸🇪", "🇨🇿", "🇯🇵"]
+}
+
+
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    Group {
+      ContentView()
+        .previewInterfaceOrientation(.landscapeRight)
+      ContentView()
+        
+        
+    }
+  }
+}
+
+
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Артур Погромский on 04.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
